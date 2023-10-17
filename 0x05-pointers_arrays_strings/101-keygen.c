a380e6acabe5fd645604d86e2b778231fef21c8d
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-17 22:25:15 +0300

    2023-10-17 22:25:14

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,13 +11,11 @@ int x;
 
 srand(time(0));
 while (x <= 2645)
-// 2645 is the sum of the ASCII codes of the characters of the password
 {
 c = rand() % 128;
 x += c;
 putchar(c);
 }
 putchar(2772 - x);
-// 2772 is the sum of the ASCII codes of the characters of the password
 return (0);
 }
