0a2ec5d0617df290d294e0a7f7c0f47a80994b90
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-10-17 20:41:49 +0300

    2023-10-17 20:41:48

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <string.h>
+
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-int _putchar(char c);
+int _putchar(char character);
 void _puts(char *str);
 void print_rev(char *s);
 
-#endif
+#endif /*MAIN_H*/
