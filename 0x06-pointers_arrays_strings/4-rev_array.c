7c81510b5055d633d8cea8945829d7a028dd1d8b
Marwa <ghariebm762@gmail.com>
2023-10-19 11:33:16 -0700

    fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reverse_array - reverses the content of an array of integers
+ * reverse_array - reversing the content of an array of integers
  * @a: array of integers
  * @n: number of elements of the array
  * Return: void
