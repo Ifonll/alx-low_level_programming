bb15cedf42a67b569dcca606974a90a9000f714d
Marwa <ghariebm762@gmail.com>
2023-10-24 10:58:54 -0700

    styling

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,14 +7,14 @@
  */
 char *_strchr(char *s, char c)
 {
-        unsigned int i;
-        
-        for (i=0; s[i] !='\0'; i++)
-        {
-                if (s[i] == c)
-                        return (&s[i]);
-        }
-        if(s[i] == c)
-                return (&s[i]);
-        return (NULL);
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	if (s[i] == c)
+		return (&s[i]);
+	return (NULL);
 }
