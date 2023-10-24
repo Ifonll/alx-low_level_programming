3be90c6d72970198aa7a44b46f101154fdc8e1cc
Marwa <ghariebm762@gmail.com>
2023-10-24 12:16:57 -0700

    me

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strspn - gets the length of a prefix sunstring
+* @s: longer string
+* @accept: shorter string
+* Return: i or the length
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+{
+	for (j = 0; accept[j] != '\0'; j++)
+{
+	if (s[i] != accept[j])
+	break;
+
+	if (accept[j] == s[i])
+	return (i);
+}
+	return (i);
+}
