c18f2b501f658c25300878f8545aa7edda85bdd5
Marwa Gharieb <ghariebm762@gmail.com>
2023-11-09 20:42:33 +0200

    a

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,7 +2,12 @@
 #include <stdarg.h>
 /**
  * Variadic function - a function that returns the sum of all its parameters
- * 
+ * va_list - a fuction in library stdarg
+ * va_satrt - a fuction in library stdarg
+ * va_end - a fuction in library stdarg
+ * @num: a parameter of integer type
+ * @i: an integer
+ * Return: 0
 */
 
 int sum(int num, ...) {
