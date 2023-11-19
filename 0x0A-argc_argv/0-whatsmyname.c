ad4c9223c9f9c38b34f4970c1df0c19b4158f572
Marwa Gharieb <ghariebm762@gmail.com>
2023-11-19 20:56:32 +0200

    comment

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Entry point for the program
+ * @argc: Argument count
+ * @argv: Argument Vector
+ * Return: 0 (success)
+ */
 
 int main(int argc, char **argv)
 {
     (void) argc;
     printf("%s", argv[0]);
+    return (0);
 }
 
