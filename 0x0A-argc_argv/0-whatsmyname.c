3090a95cfb0c696e729f373d6adc42d3464e338d
Ifonll <142951270+Ifonll@users.noreply.github.com>
2023-11-22 01:23:05 +0200

    Update 0-whatsmyname.c
    k

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - Entry point for the program
  * @argc: Argument count
  * @argv: Argument Vector
  * Return: 0 (success)
  */
+
 int main(int argc, char **argv)
 {
     (void) argc;
